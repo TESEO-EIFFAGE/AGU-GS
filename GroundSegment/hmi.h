c26e11c5f9166cea36255dd8194acefb0c26906f
Marco Piccolino <marco.piccolino@orbyta.it>
2022-09-01 14:27:14 +0200

    UI - expose telemetry status

--- GroundSegment/hmi.h
+++ GroundSegment/hmi.h
@@ -49,6 +49,41 @@ public:
     Q_PROPERTY(int TelemetryStatusMask MEMBER m_TelemetryStatusMask NOTIFY TelemetryStatusMaskChanged)
     Q_PROPERTY(int NumberOfGPSSatellite MEMBER m_NumberOfGPSSatellite NOTIFY NumberOfGPSSatelliteChanged)
 
+    Q_PROPERTY(bool telemetry0 MEMBER m_telemetry0 NOTIFY telemetry0Changed)
+    Q_PROPERTY(bool telemetry1 MEMBER m_telemetry1 NOTIFY telemetry1Changed)
+    Q_PROPERTY(bool telemetry2 MEMBER m_telemetry2 NOTIFY telemetry2Changed)
+    Q_PROPERTY(bool telemetry3 MEMBER m_telemetry3 NOTIFY telemetry3Changed)
+    Q_PROPERTY(bool telemetry4 MEMBER m_telemetry4 NOTIFY telemetry4Changed)
+    Q_PROPERTY(bool telemetry5 MEMBER m_telemetry5 NOTIFY telemetry5Changed)
+    Q_PROPERTY(bool telemetry6 MEMBER m_telemetry6 NOTIFY telemetry6Changed)
+    Q_PROPERTY(bool telemetry7 MEMBER m_telemetry7 NOTIFY telemetry7Changed)
+    Q_PROPERTY(bool telemetry8 MEMBER m_telemetry8 NOTIFY telemetry8Changed)
+    Q_PROPERTY(bool telemetry9 MEMBER m_telemetry9 NOTIFY telemetry9Changed)
+    Q_PROPERTY(bool telemetry10 MEMBER m_telemetry10 NOTIFY telemetry10Changed)
+    Q_PROPERTY(bool telemetry11 MEMBER m_telemetry11 NOTIFY telemetry11Changed)
+    Q_PROPERTY(bool telemetry12 MEMBER m_telemetry12 NOTIFY telemetry12Changed)
+    Q_PROPERTY(bool telemetry13 MEMBER m_telemetry13 NOTIFY telemetry13Changed)
+    Q_PROPERTY(bool telemetry14 MEMBER m_telemetry14 NOTIFY telemetry14Changed)
+    Q_PROPERTY(bool telemetry15 MEMBER m_telemetry15 NOTIFY telemetry15Changed)
+    Q_PROPERTY(bool telemetry16 MEMBER m_telemetry16 NOTIFY telemetry16Changed)
+    Q_PROPERTY(bool telemetry17 MEMBER m_telemetry17 NOTIFY telemetry17Changed)
+    Q_PROPERTY(bool telemetry18 MEMBER m_telemetry18 NOTIFY telemetry18Changed)
+    Q_PROPERTY(bool telemetry19 MEMBER m_telemetry19 NOTIFY telemetry19Changed)
+    Q_PROPERTY(bool telemetry20 MEMBER m_telemetry20 NOTIFY telemetry20Changed)
+    Q_PROPERTY(bool telemetry21 MEMBER m_telemetry21 NOTIFY telemetry21Changed)
+    Q_PROPERTY(bool telemetry22 MEMBER m_telemetry22 NOTIFY telemetry22Changed)
+    Q_PROPERTY(bool telemetry23 MEMBER m_telemetry23 NOTIFY telemetry23Changed)
+    Q_PROPERTY(bool telemetry24 MEMBER m_telemetry24 NOTIFY telemetry24Changed)
+    Q_PROPERTY(bool telemetry25 MEMBER m_telemetry25 NOTIFY telemetry25Changed)
+    Q_PROPERTY(bool telemetry26 MEMBER m_telemetry26 NOTIFY telemetry26Changed)
+    Q_PROPERTY(bool telemetry27 MEMBER m_telemetry27 NOTIFY telemetry27Changed)
+    Q_PROPERTY(bool telemetry28 MEMBER m_telemetry28 NOTIFY telemetry28Changed)
+    Q_PROPERTY(bool telemetry29 MEMBER m_telemetry29 NOTIFY telemetry29Changed)
+    Q_PROPERTY(bool telemetry30 MEMBER m_telemetry30 NOTIFY telemetry30Changed)
+    Q_PROPERTY(bool telemetry31 MEMBER m_telemetry31 NOTIFY telemetry31Changed)
+
+
+
     //Ui::MainWindow myui;
     //TopDialog TopDialogWindow;
 
@@ -136,6 +171,70 @@ signals:
 
     void NumberOfGPSSatelliteChanged();
 
+    void telemetry0Changed();
+
+    void telemetry1Changed();
+
+    void telemetry2Changed();
+
+    void telemetry3Changed();
+
+    void telemetry4Changed();
+
+    void telemetry5Changed();
+
+    void telemetry6Changed();
+
+    void telemetry7Changed();
+
+    void telemetry8Changed();
+
+    void telemetry9Changed();
+
+    void telemetry10Changed();
+
+    void telemetry11Changed();
+
+    void telemetry12Changed();
+
+    void telemetry13Changed();
+
+    void telemetry14Changed();
+
+    void telemetry15Changed();
+
+    void telemetry16Changed();
+
+    void telemetry17Changed();
+
+    void telemetry18Changed();
+
+    void telemetry19Changed();
+
+    void telemetry20Changed();
+
+    void telemetry21Changed();
+
+    void telemetry22Changed();
+
+    void telemetry23Changed();
+
+    void telemetry24Changed();
+
+    void telemetry25Changed();
+
+    void telemetry26Changed();
+
+    void telemetry27Changed();
+
+    void telemetry28Changed();
+
+    void telemetry29Changed();
+
+    void telemetry30Changed();
+
+    void telemetry31Changed();
+
 private:
 
     uint64_t m_TimeStamp{};
@@ -174,6 +273,39 @@ private:
     float m_Quaternion3{};
     uint64_t m_TelemetryStatusMask{};
     uint8_t m_NumberOfGPSSatellite{};
+
+    bool m_telemetry0;
+    bool m_telemetry1;
+    bool m_telemetry2;
+    bool m_telemetry3;
+    bool m_telemetry4;
+    bool m_telemetry5;
+    bool m_telemetry6;
+    bool m_telemetry7;
+    bool m_telemetry8;
+    bool m_telemetry9;
+    bool m_telemetry10;
+    bool m_telemetry11;
+    bool m_telemetry12;
+    bool m_telemetry13;
+    bool m_telemetry14;
+    bool m_telemetry15;
+    bool m_telemetry16;
+    bool m_telemetry17;
+    bool m_telemetry18;
+    bool m_telemetry19;
+    bool m_telemetry20;
+    bool m_telemetry21;
+    bool m_telemetry22;
+    bool m_telemetry23;
+    bool m_telemetry24;
+    bool m_telemetry25;
+    bool m_telemetry26;
+    bool m_telemetry27;
+    bool m_telemetry28;
+    bool m_telemetry29;
+    bool m_telemetry30;
+    bool m_telemetry31;
 };
 
 #endif // HMI_H
