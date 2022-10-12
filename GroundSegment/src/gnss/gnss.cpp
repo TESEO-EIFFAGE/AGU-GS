2454f6aa8770f45461d54c4e590f26c7e135a10a
Nicolo Ricotta <nicolo.ricotta@orbyta.it>
2022-10-12 06:01:49 -0400

    Added Comments to file gnss.cpp

--- GroundSegment/src/gnss/gnss.cpp
+++ GroundSegment/src/gnss/gnss.cpp
@@ -3,6 +3,11 @@
 #include <gnss.h>
 #include "gps_driver/drotek_f9p_rover.cpp"
 
+/*!
+        \class GNSS
+
+        \brief The GNSS class define the gnss object and its communication
+*/
 GNSS::GNSS(QObject *parent) :
     QObject(parent)
 {
@@ -27,7 +32,11 @@ GNSS::GNSS(QObject *parent) :
         std::cout << "Please make sure that the application is running as root and that the GNSS device is properly plugged." << std::endl;
     }
 }
+/*!
+    \fn void GNSS::UpdateData()
 
+    It update gnss position and time
+*/
 void GNSS::UpdateData() {
     double hasFix;
 
@@ -48,37 +57,75 @@ void GNSS::UpdateData() {
     delete [ ] gnss_info;
 }
 
+/*!
+    \fn double GNSS::latitude() const
 
+    It return latitude value
+*/
 double GNSS::latitude() const
 {
     return m_latitude;
 }
+/*!
+    \fn double GNSS::longitude() const
 
+    It return longitude value
+*/
 double GNSS::longitude() const
 {
     return m_longitude;
 }
+/*!
+    \fn double GNSS::altitude() const
+
+    It return altitude value
+*/
 double GNSS::altitude() const
 {
     return m_altitude;
 }
+/*!
+    \fn bool GNSS::hasFix() const
+
+    It return hasFix value
+*/
 bool GNSS::hasFix() const
 {
     return m_hasFix;
 }
+/*!
+    \fn int GNSS::hour() const
+
+    It return hour value
+*/
 int GNSS::hour() const
 {
     return m_hour;
 }
+/*!
+    \fn int GNSS::minute() const
+ hasFix
+    It return minute value
+*/
 int GNSS::minute() const
 {
     return m_minute;
 }
+/*!
+    \fn double GNSS::second() const
+
+    It return second value
+*/
 double GNSS::second() const
 {
     return m_second;
 }
 
+/*!
+    \fn void GNSS::setLatitude(const double latitude)
+
+    It set new value for latitude
+*/
 void GNSS::setLatitude(const double latitude)
 {
     if (latitude == m_latitude)
@@ -87,7 +134,11 @@ void GNSS::setLatitude(const double latitude)
     m_latitude = latitude;
     emit latitudeChanged();
 }
+/*!
+    \fn void GNSS::setLongitude(const double longitude)
 
+    It set new value for longitude
+*/
 void GNSS::setLongitude(const double longitude)
 {
     if (longitude == m_longitude)
@@ -96,7 +147,11 @@ void GNSS::setLongitude(const double longitude)
     m_longitude = longitude;
     emit longitudeChanged();
 }
+/*!
+    \fn void GNSS::setAltitude(const double altitude)
 
+    It set new value for altitude
+*/
 void GNSS::setAltitude(const double altitude)
 {
     if (altitude == m_altitude)
@@ -105,7 +160,11 @@ void GNSS::setAltitude(const double altitude)
     m_altitude = altitude;
     emit altitudeChanged();
 }
+/*!
+    \fn void GNSS::setHasFix(const bool hasFix)
 
+    It set new value for hasFix
+*/
 void GNSS::setHasFix(const bool hasFix)
 {
     if (hasFix == m_hasFix)
@@ -114,7 +173,11 @@ void GNSS::setHasFix(const bool hasFix)
     m_hasFix = hasFix;
     emit hasFixChanged();
 }
+/*!
+    \fn void GNSS::setHour(const int hour)
 
+    It set new value for hour
+*/
 void GNSS::setHour(const int hour)
 {
     if (hour == m_hour)
@@ -123,7 +186,11 @@ void GNSS::setHour(const int hour)
     m_hour = hour;
     emit hourChanged();
 }
+/*!
+    \fn void GNSS::setMinute(const int minute)
 
+    It set new value for minute
+*/
 void GNSS::setMinute(const int minute)
 {
     if (minute == m_minute)
@@ -132,7 +199,11 @@ void GNSS::setMinute(const int minute)
     m_minute = minute;
     emit minuteChanged();
 }
+/*!
+    \fn void GNSS::setSecond(const double second)
 
+    It set new value for second
+*/
 void GNSS::setSecond(const double second)
 {
     if (second == m_second)
