b6c3ac4fedec05ab55151f232e44722726b246c7
Marco Piccolino <marco.piccolino@orbyta.it>
2022-11-25 10:50:19 +0100

    added check on cmd arguments

--- GroundSegment/main.cpp
+++ GroundSegment/main.cpp
@@ -29,50 +29,50 @@ int main(int argc, char *argv[])
     //MainWindow w;
     QByteArray data  = "FD77000000FFAA000011"
                        "000102030405060708090A0B0C0D0E0F"   /*  P  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  A  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  Y  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  L  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  O  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  A  */
-                       "000102030405060708090A0B0C0D0E0F"   /*  D  */
-                       "00010203040506"
+            "000102030405060708090A0B0C0D0E0F"   /*  A  */
+            "000102030405060708090A0B0C0D0E0F"   /*  Y  */
+            "000102030405060708090A0B0C0D0E0F"   /*  L  */
+            "000102030405060708090A0B0C0D0E0F"   /*  O  */
+            "000102030405060708090A0B0C0D0E0F"   /*  A  */
+            "000102030405060708090A0B0C0D0E0F"   /*  D  */
+            "00010203040506"
                        "59E7";
 
-//    QByteArray data  = "FD010000000101010101010D3D";
+    //    QByteArray data  = "FD010000000101010101010D3D";
 
-//    QSerialPort *Serial0 = new QSerialPort();
+    //    QSerialPort *Serial0 = new QSerialPort();
 
     // Serial0 PORTA CHE TRASMETTE
 
-//    Serial0->setPortName("/dev/ttyUSB0");
-//    Serial0->setParity(QSerialPort::NoParity);
-//    Serial0->setBaudRate(QSerialPort::Baud115200);
-//    Serial0->setStopBits(QSerialPort::OneStop);
-//    Serial0->setFlowControl(QSerialPort::NoFlowControl);
-//    Serial0->open(QIODevice::ReadWrite);
+    //    Serial0->setPortName("/dev/ttyUSB0");
+    //    Serial0->setParity(QSerialPort::NoParity);
+    //    Serial0->setBaudRate(QSerialPort::Baud115200);
+    //    Serial0->setStopBits(QSerialPort::OneStop);
+    //    Serial0->setFlowControl(QSerialPort::NoFlowControl);
+    //    Serial0->open(QIODevice::ReadWrite);
 
 
-//    QProcess flight_segment_simulator;
-//    flight_segment_simulator.setProgram("libs/F9P-Viewer/simulate_flying_vehicle/simulate_flying_vehicle");
-//    flight_segment_simulator.start();
+    //    QProcess flight_segment_simulator;
+    //    flight_segment_simulator.setProgram("libs/F9P-Viewer/simulate_flying_vehicle/simulate_flying_vehicle");
+    //    flight_segment_simulator.start();
 
-//    QTimer timer;
-//    QObject::connect(&timer, &QTimer::timeout, [Serial0]() { Serial0->write("000000010100101001"); });
-//    timer.start(500);
+    //    QTimer timer;
+    //    QObject::connect(&timer, &QTimer::timeout, [Serial0]() { Serial0->write("000000010100101001"); });
+    //    timer.start(500);
     /* PROVA */
     //Serial0->write(data);
 
-     //w.show();
+    //w.show();
 
     QQmlApplicationEngine engine;
     auto gsCore = new GSCore(&app);
     auto hmi = gsCore->hmi();
     auto gpsData = gsCore->gnss();
-
-    gsCore->setGNSSPort(QCoreApplication::arguments().at(1));
-    gsCore->setRLPort(QCoreApplication::arguments().last());
-
-
+    if(QCoreApplication::arguments().at(1) != "" && QCoreApplication::arguments().last() != "")
+    {
+        gsCore->setGNSSPort(QCoreApplication::arguments().at(1));
+        gsCore->setRLPort(QCoreApplication::arguments().last());
+    }
     engine.rootContext()->setContextProperty("hmi", hmi);
     engine.rootContext()->setContextProperty("gpsData", gpsData);
 
